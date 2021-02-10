af86c984af7d8a062ab45cc6c6453cc7fd40f1af
loris2222 <loris2222@hotmail.it>
2021-02-10 09:28:09 +0100

    Changed loss to euclidean distance

--- code/voc_resnet50.py
+++ code/voc_resnet50.py
@@ -9,6 +9,12 @@ from keras.models import Sequential, Model, load_model
 from keras.optimizers import SGD, Adam
 from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
 from keras import metrics
+import keras.backend as K
+
+
+def euclidean_distance_loss(y_true, y_pred):
+    return K.sqrt(K.sum(K.square(y_pred - y_true), axis=-1))
+
 
 class_names = importdataset.CLASS_NAMES
 X_train, X_test, Y_train, Y_test = importdataset.load_dataset()
@@ -25,15 +31,15 @@ base_model.trainable = False
 #predictions = Dense(num_classes, activation='sigmoid')(x)
 #model = Model(inputs=base_model.input, outputs=predictions)
 
-inputs = Input(shape=(150, 150, 3))
+inputs = Input(shape=(img_height, img_width, 3))
 x = base_model(inputs, training=False)
 x = GlobalAveragePooling2D()(x)
 outputs = Dense(num_classes, activation='sigmoid')(x)
 model = Model(inputs, outputs)
 
 model.compile(optimizer=Adam(),
-              loss=BinaryCrossentropy(from_logits=True),
-              metrics=[metrics.BinaryAccuracy()])
+              loss=euclidean_distance_loss,
+              metrics=[metrics.BinaryAccuracy(), euclidean_distance_loss])
 model.fit(X_train, Y_train, epochs=2, batch_size=64)
 
 preds = model.evaluate(X_test, Y_test)
