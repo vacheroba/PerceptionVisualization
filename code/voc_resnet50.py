f77ef3e0ee067fd6776938e4edcd1ae192a600a8
loris2222 <loris2222@hotmail.it>
2021-02-10 08:36:11 +0100

    Frozen resnet weights

--- code/voc_resnet50.py
+++ code/voc_resnet50.py
@@ -3,11 +3,12 @@ import numpy as np
 from PIL import Image
 import os
 import importdataset
-from keras import applications
+from keras import applications, Input
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, GlobalAveragePooling2D
 from keras.models import Sequential, Model, load_model
 from keras.optimizers import SGD, Adam
 from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
+from keras import metrics
 
 class_names = importdataset.CLASS_NAMES
 X_train, X_test, Y_train, Y_test = importdataset.load_dataset()
@@ -17,14 +18,22 @@ num_classes = 20
 
 base_model = applications.resnet50.ResNet50(weights='imagenet', include_top=False,
                                             input_shape=(img_height, img_width, 3))
+base_model.trainable = False
 
-x = base_model.output
+#x = base_model.output
+#x = GlobalAveragePooling2D()(x)
+#predictions = Dense(num_classes, activation='sigmoid')(x)
+#model = Model(inputs=base_model.input, outputs=predictions)
+
+inputs = Input(shape=(150, 150, 3))
+x = base_model(inputs, training=False)
 x = GlobalAveragePooling2D()(x)
-predictions = Dense(num_classes, activation='sigmoid')(x)
-model = Model(inputs=base_model.input, outputs=predictions)
+outputs = Dense(num_classes, activation='sigmoid')(x)
+model = Model(inputs, outputs)
 
-adam = Adam(lr=0.0001)
-model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['mse', 'categorical_crossentropy'])
+model.compile(optimizer=Adam(),
+              loss=BinaryCrossentropy(from_logits=True),
+              metrics=[metrics.BinaryAccuracy()])
 model.fit(X_train, Y_train, epochs=2, batch_size=64)
 
 preds = model.evaluate(X_test, Y_test)
