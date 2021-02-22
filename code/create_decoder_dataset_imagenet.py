b0c3b1c8d40bd82bf0e0f16ff8b9048a038bd45b
loris2222 <loris2222@hotmail.it>
2021-02-22 11:23:29 +0100

    Changed from B7 to B0 for performance reasons

--- code/create_decoder_dataset_imagenet.py
+++ code/create_decoder_dataset_imagenet.py
@@ -26,7 +26,7 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 basepath = os.getcwd()
 imagenetpath = os.path.join(basepath, "../datasets/ImageNet/imagenet_object_localization_patched2019.tar/ILSVRC/Data/CLS-LOC/test")
 
-base_model = tf.keras.applications.EfficientNetB7(
+base_model = tf.keras.applications.EfficientNetB0(
     include_top=True,
     weights="imagenet",
     input_tensor=None,
@@ -41,27 +41,37 @@ del base_model
 
 encoder.summary()
 
-num_images = 4000
+num_images = 20000
 
-X_train = np.zeros([num_images, 600, 600, 3], dtype=np.uint8)
-E_train = np.zeros([num_images, 19, 19, 2560], dtype=np.float32)
+# For B7
+# X_train = np.zeros([num_images, 600, 600, 3], dtype=np.uint8)
+# E_train = np.zeros([num_images, 19, 19, 2560], dtype=np.float32)
+
+# For B0
+X_train = np.zeros([num_images, 224, 224, 3], dtype=np.uint8)
+E_train = np.zeros([num_images, 7, 7, 1280], dtype=np.float32)
+
 print("Loading images")
 image_paths = os.listdir(imagenetpath)
 
+# img_size = 600
+img_size = 224
+
 for i in range(0, num_images):
-    if i % 100 == 0:
+    if i % 100 == 0 and i > 0:
+        E_train[i-100:i, :, :, :] = encoder.predict(X_train[i-100:i, :, :, :])
         print(i)
     img = Image.open(os.path.join(imagenetpath, image_paths[i]))
-    img = img.resize((600, 600))
+    img = img.resize((img_size, img_size))
     imgarray = np.array(img)
-    if imgarray.shape == (600, 600):
-        h = np.zeros([600, 600, 3]).astype(np.uint8)
+    if imgarray.shape == (img_size, img_size):
+        h = np.zeros([img_size, img_size, 3]).astype(np.uint8)
         h[:, :, 0] = imgarray
         h[:, :, 1] = imgarray
         h[:, :, 2] = imgarray
         imgarray = h
     X_train[i, :, :, :] = imgarray
-    E_train[i, :, :, :] = encoder.predict(X_train[i:i+1, :, :, :])
+    # E_train[i, :, :, :] = encoder.predict(X_train[i:i+1, :, :, :])
 
 # Gets outputs of modified model
 print("Predicting")
@@ -72,7 +82,7 @@ print(X_train.shape)
 
 # Saves in h5
 print("Saving result")
-datasetpath = os.path.join(basepath, "../datasets/dataset_encoder_imagenet_onlyimg.h5")
+datasetpath = os.path.join(basepath, "../datasets/dataset_encoder_imagenet.h5")
 hf = h5py.File(datasetpath, 'w')
 hf.create_dataset('X_train', data=X_train)
 hf.create_dataset('E_train', data=E_train)
